c8a8aa364a47709bb48c0e6c1cca61e052830215
Игорь <igor@Igors-MacBook-Air.local>
2023-06-05 17:02:42 +0300

    Основа проекта - этап 1

---
+++ AuthServer.Web/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Web.Controllers
+{
+	public class AuthController : RootController
+	{
+		public AuthController()
+		{
+		}
+
+        [HttpGet]
+        public void Login()
+		{
+
+		}
+	}
+}
+
--- AuthServer.Web/Program.cs
+++ AuthServer.Web/Program.cs
@@ -1,15 +1,64 @@
+using System.Reflection;
+using System.Linq;
+using NLog.Extensions.Logging;
+
 namespace AuthServer.Web
 {
     public class Program
     {
+        private static NLog.Logger _log = NLog.LogManager.GetCurrentClassLogger();
+
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
-            app.MapGet("/", () => "Hello World!");
+                // Выводим информацию о загруженных сборках
+                ShowDependencies();
+                // Меняем провайдер логирования на Nlog
+                СonfigureLogging(builder);
+                // Добавляем контроллеры
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+     
 
-            app.Run();
+                var app = builder.Build();
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                // Маппим контроллеры
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                _log.Fatal(ex);
+            }
+        }
+
+        private static void ShowDependencies()
+        {
+            var dependencies = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (var dependency in dependencies)
+            {
+                Console.WriteLine($"{dependency.GetName().Name} ({dependency.GetName().Version})");
+            }
+        }
+
+        private static void СonfigureLogging(WebApplicationBuilder builder)
+        {
+            builder.Logging
+                .ClearProviders()
+                .SetMinimumLevel(LogLevel.Trace)
+                .AddNLog();
         }
     }
 }
